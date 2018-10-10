9fd4a55e0f46ccee98c571c5e9bd0e3f308ba978
Myracle <max.weber.consultant@outlook.com>
2018-10-10 14:38:45 +0900

    Repaired fa5 so it uses the newest version of fontawesome

--- src/js/fa5.js
+++ src/js/fa5.js
@@ -1,12 +1,17 @@
 // FontAwesome 5
-import fontawesome from '@fortawesome/fontawesome';
+import {
+  library,
+  dom
+} from '@fortawesome/fontawesome-free'
 
 // FontAwesome 5 Icon Sets
-import solid from '@fortawesome/fontawesome-free-solid';
-import regular from '@fortawesome/fontawesome-free-regular';
-import brands from '@fortawesome/fontawesome-free-brands';
+import solid from '@fortawesome/fontawesome-free/js/solid';
+import regular from '@fortawesome/fontawesome-free/js/regular';
+import brands from '@fortawesome/fontawesome-free/js/brands';
 
 // Populate FontAwesome 5 icon library
-fontawesome.library.add(solid);
-fontawesome.library.add(regular);
-fontawesome.library.add(brands);
+library.add(solid);
+library.add(regular);
+library.add(brands);
+
+dom.watch();
