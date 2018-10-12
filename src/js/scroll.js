14fc0650aa33ac2a1505b3d2c44af32aa768dba0
Myracle <max.weber.consultant@outlook.com>
2018-10-12 22:13:58 +0900

    Fix Scroll was wrong

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,6 +1,6 @@
 // Scroll to first element
 $('#landing-page-down span').click(function () {
-  var scrollDistance = $('#landing-page').next().offset().top;
+  var scrollDistance = ($('#landing-page').next().offset().top) - 70;
   $('html, body').animate({
     scrollTop: scrollDistance + 'px'
   }, 500);
